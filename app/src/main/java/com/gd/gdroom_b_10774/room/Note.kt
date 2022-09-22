e7a2e693298d2f20fb79740b72a259c96c3e0743
DanielAzriel <danielaazrl@gmail.com>
2022-09-22 07:38:21 +0700

    GDRoom_B_10774

---
+++ app/src/main/java/com/gd/gdroom_b_10774/room/Note.kt
@@ -0,0 +1,12 @@
+package com.gd.gdroom_b_10774.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String
+)
